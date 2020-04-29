e68ef64ccd35a6f6283ae660e50528be87752000
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-04-29 23:03:03 +0200

    Implement basic feature: set nickname and players number
    Change players object structure
    Implement function to get id, nickname or index from players
    Adding ids object in players for fastest search
    Setchoice let first player to select number of player
    sendCards send player specific deck
    nextPlayer send event to next player to play a card
    Rename clear to reset
    Change ready timeout to 2000ms for testing

--- server/server.js
+++ server/server.js
@@ -11,30 +11,72 @@ app.use(express.static(clientPath));
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
-let players = { order: [], playerCount: 0 };
+let players = { order: [], playerCount: 0, deck: [], currentChoice: { player: "", choice: "" }, ids: {} };
 
-const initPlayers = (pseudo, socket) => {
+function idFromPseudo(pseudo) {
+  return players[pseudo].id;
+}
+function pseudoFromId(id) {
+  return players.ids[id].player;
+}
+function indexFromArray(array, val) {
+  return array.findIndex(el => el === val);
+}
+
+function initPlayers(pseudo, socket) {
   if (players[pseudo]) {
+    delete players.ids[idFromPseudo(pseudo)];
     players[pseudo].id = socket.id;
+    players.ids[socket.id] = { player: pseudo };
   } else {
     players[pseudo] = { id: socket.id, cards: [] };
+    players.ids[socket.id] = { player: pseudo };
   }
   if (players.order.includes(pseudo)) {
-    //rejoinParty();
     console.log("Déjà en partie");
   } else {
     players.order.push(pseudo);
   }
   io.emit("waitingPlayers", players.order);
-};
+}
 
-const sendCards = () => {
+function setChoice(choix) {
+  players.currentChoice = choix; //choix {player,choice}
+
+  let pseudoIndex = indexFromArray(players.order, choix.player);
+  if (pseudoIndex + 1 === players.order.length) {
+    return startGame();
+  }
+  let nextPlayer = players.order[pseudoIndex + 1];
+  io.to(idFromPseudo(nextPlayer)).emit("choice", players.currentChoice);
+}
+
+function sendCards() {
   let playerDeck = cards.getPlayerDeck();
   for (let i = 0; i < players.order.length; i++) {
     let player = players.order[i];
-    io.to(players[player].id).emit("yourCards", playerDeck[i]);
+    io.to(idFromPseudo(player)).emit("yourCards", playerDeck[i]);
   }
-};
+  io.to(idFromPseudo(players.order[0])).emit("choice", players.currentChoice.choice);
+}
+
+function startGame() {
+  io.emit("startGame");
+  io.to(idFromPseudo(players.order[0])).emit("yourTurn");
+  currentPlayer = players.order[0];
+  console.log("start");
+}
+
+function nextPlayer(pseudo) {
+  let currentIndex = indexFromArray(players.order, pseudo);
+  if (currentIndex + 1 === players.order.length) {
+    currentIndex = 0;
+    io.emit("clean");
+  } else {
+    currentIndex++;
+  }
+  io.to(idFromPseudo(players.order[currentIndex])).emit("yourTurn");
+}
 
 io.on("connection", socket => {
   socket.on("sendPlayerNumber", num => {
@@ -46,7 +88,7 @@ io.on("connection", socket => {
   });
 
   socket.on("cardPlayed", card => {
-    socket.broadcast.emit("cardPlayed", card);
+    socket.broadcast.emit("cardPlayed", card, pseudoFromId(socket.id));
   });
 
   // get pseudo from client
@@ -57,7 +99,8 @@ io.on("connection", socket => {
       io.emit("logToAll", "Welcome to " + pseudo);
       if (players.order.length === players.playerCount) {
         io.emit("ready");
-        setTimeout(sendCards, 11000);
+        // setTimeout(sendCards, 11000);
+        setTimeout(sendCards, 3000);
       }
     } else {
       socket.emit("tooManyPlayers");
@@ -72,7 +115,19 @@ io.on("connection", socket => {
     console.log(players);
   });
 
-  socket.on("clear", () => (players = { order: [], playerCount: 0 }));
+  socket.on(
+    "reset",
+    () => (players = { order: [], playerCount: 0, deck: [], currentChoice: { player: "", choice: "" }, ids: {} })
+  );
+
+  socket.on("sendCard", cardPos => {
+    let deck = [players.deck, players[pseudoFromId(socket.id)].cards[cardPos]];
+    players.deck.push(deck);
+    players[pseudoFromId(socket.id)].cards.splice(cardPos, 1);
+    nextPlayer(pseudoFromId(socket.id));
+  });
+
+  socket.on("setChoice", setChoice);
 });
 
 //server error detection
