8cf6188f22275026496bf7255201c83849d69262
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-04-29 23:09:05 +0200

    React from server event
    Remove width calculation
    SendCard to other player and show it on screen
    Display players nickname
    Player must now enter a non-empty nickname
    movePlayer sort players array to move player at the begining
    drawPseudo only at the begining of the game
    Rename function clear to reset
    Adding choice buttons to set player choice and send to the server
    clean event remove only the played cards

--- client/src/client.js
+++ client/src/client.js
@@ -1,58 +1,42 @@
-let cardWidth = window.innerWidth / 10;
-let cardHeight = cardWidth * 1.92;
-let cardGap = (5 * cardWidth) / 4;
+let players = [];
+let pseudo = "";
 
 function initArea(cards) {
   let gameArea = document.createElement("div");
-  gameArea.style.height = cardHeight.toString() + "px";
   gameArea.className = "area";
 
-  //calculate space for positioning card
-  let space = (0.8 * window.innerWidth) / (cards.length + 1);
-
   for (let i = 0; i < cards.length; i++) {
-    let pos = space * (i + 1) - cardWidth / 2;
-
-    //calculate position for low number of cards
-    if (space > cardGap) {
-      let totWidth = cardGap * cards.length;
-      let posOri = (0.8 * window.innerWidth - totWidth) / 2;
-      pos = posOri + cardGap * i;
-    }
-
-    let card = document.createElement("div");
-    card.style.width = cardWidth.toString() + "px";
-    card.style.height = cardHeight.toString() + "px";
-    card.style.left = pos.toString() + "px";
-    card.style.position = "absolute";
-
     let cardHere = { rank: cards[i][0], value: cards[i][1] };
 
     let img = document.createElement("img");
     img.className = "card";
-    img.style.borderRadius = (cardWidth / 10).toString() + "px";
-    img.id = cardHere.rank + "," + cardHere.value;
     img.src = "../assets/" + cardHere.rank + cardHere.value + ".jpg";
     img.alt = cardHere.rank + cardHere.value;
     img.onclick = () => {
-      sendCard(cardHere);
+      if (!img.hasAttribute("disabled")) {
+        if (gameArea.className === "area playerTurn") {
+          sendCard(cardHere);
+          img.remove();
+          socket.emit("sendCard", i);
+        }
+      }
     };
 
-    card.appendChild(img);
-
-    gameArea.appendChild(card);
+    gameArea.appendChild(img);
   }
-  let frag = document.createDocumentFragment();
-  frag.appendChild(gameArea);
-  document.body.appendChild(frag);
+  document.body.appendChild(gameArea);
+  return gameArea;
 }
 
-const drawPseudo = pseudo => {
-  let pseudoText = document.createElement("div");
-  pseudoText.className = "text";
-  pseudoText.style.textAlign = "left";
-  pseudoText.innerText = pseudo;
-  document.body.appendChild(pseudoText);
+const drawPseudo = () => {
+  let frag = document.createDocumentFragment();
+  for (let i = 1; i < players.length; i++) {
+    let pseudoText = document.createElement("div");
+    pseudoText.className = "text pseudoText pseudo" + (i + 1).toString();
+    pseudoText.innerText = players[i];
+    frag.appendChild(pseudoText);
+  }
+  document.body.appendChild(frag);
 };
 
 const waitingPlayer = waitingPlayers => {
@@ -60,6 +44,7 @@ const waitingPlayer = waitingPlayers => {
   if (temp !== null) {
     temp.remove();
   }
+  players = waitingPlayers;
   let waiting = document.createElement("div");
   waiting.id = "waiting";
   waiting.className = "text";
@@ -77,25 +62,38 @@ const initPseudo = () => {
   input.type = "text";
   input.id = "inputPseudo";
   input.placeholder = "Entrer votre pseudo";
+  input.required = true;
+  input.oninvalid = () => {
+    input.setCustomValidity("Entrez un pseudo!");
+  };
+  input.oninput = () => {
+    input.setCustomValidity("");
+  };
+  input.onkeyup = event => {
+    if (event.keyCode === 13) {
+      button.click();
+    }
+  };
+
   let button = document.createElement("button");
   button.innerText = "Valider";
   button.className = "btn btn-primary";
   button.id = "btnPseudo";
   button.type = "submit";
   button.onclick = () => {
-    socket.emit("sendPseudo", input.value);
-    input.remove();
-    button.remove();
-    drawPseudo(input.value);
-  };
-  document.body.appendChild(input);
-  document.body.appendChild(button);
-
-  document.getElementById("inputPseudo").addEventListener("keyup", event => {
-    if (event.keyCode === 13) {
-      document.getElementById("btnPseudo").click();
+    if (input.checkValidity()) {
+      socket.emit("sendPseudo", input.value);
+      input.remove();
+      button.remove();
+      pseudo = input.value;
+    } else {
+      input.focus();
     }
-  });
+  };
+  let form = document.createElement("form");
+  form.appendChild(input);
+  form.appendChild(button);
+  document.body.appendChild(form);
 };
 
 function setPlayerNumber() {
@@ -121,29 +119,24 @@ function setPlayerNumber() {
   document.body.appendChild(divButtons);
 }
 
-function drawPlayerCard(card) {
-  let img = document.createElement("img");
-  let div = document.createElement("div");
-  div.appendChild(img);
-  div.style.width = cardWidth.toString() + "px";
-  div.style.height = cardHeight.toString() + "px";
-  div.style.position = "absolute";
-  div.style.left = "50%";
-  div.style.top = "50%";
-  div.style.transform = "translate(-50%,-50%)";
-
-  img.className = "card unclickable";
-  img.style.borderRadius = (cardWidth / 10).toString() + "px";
-  img.id = card.rank + "," + card.value;
-  img.src = "../assets/" + card.rank + card.value + ".jpg";
-  img.alt = card.rank + card.value;
-  document.body.appendChild(div);
-  setTimeout(() => div.remove(), 2000);
+function movePlayers(pos) {
+  let temp = [];
+  for (let i = 0; i < players.length; i++) {
+    temp.push(players[(i + pos) % players.length]);
+  }
+  players = temp;
 }
 
-//setPlayerNumber();
+function drawPlayerCard(card, pseudoCard) {
+  let playerIndex = players.findIndex(val => val === pseudoCard);
 
-//initId();
+  let img = document.createElement("img");
+  img.className = "otherCard player" + (playerIndex + 1).toString();
+  img.src = "../assets/" + card.rank + card.value + ".jpg";
+  img.alt = card.rank + card.value;
+
+  document.body.appendChild(img);
+}
 
 const socket = io();
 
@@ -166,6 +159,9 @@ socket.on("tooManyPlayers", () => {
 socket.on("logToAll", message => console.log(`%c${message}`, "color: green"));
 
 socket.on("ready", () => {
+  let playerPos = players.findIndex(val => val === pseudo);
+  movePlayers(playerPos);
+
   document.getElementById("waiting").remove();
   let starting = document.createElement("div");
   starting.className = "text";
@@ -174,7 +170,8 @@ socket.on("ready", () => {
   starting.style.top = "50%";
   starting.style.transform = "translate(-50%, 0%)";
   document.body.appendChild(starting);
-  let secondes = 10;
+  // let secondes = 10;
+  let secondes = 2;
   starting.innerText = "Le jeu commence dans " + secondes.toString() + " secondes...";
   setInterval(() => {
     secondes--;
@@ -182,6 +179,7 @@ socket.on("ready", () => {
       starting.innerText = "Le jeu commence";
       setTimeout(() => {
         starting.remove();
+        drawPseudo();
       }, 1000);
       clearInterval(this);
     }
@@ -193,14 +191,73 @@ function push() {
   socket.emit("logPlayers");
 }
 
-function clear() {
-  socket.emit("clear");
+function reset() {
+  socket.emit("reset");
 }
 
 function sendCard(card) {
   socket.emit("cardPlayed", card);
+  document.getElementsByClassName("area playerTurn")[0].className = "area";
+  let img = document.createElement("img");
+  img.className = "otherCard playerCard";
+  img.src = "../assets/" + card.rank + card.value + ".jpg";
+  document.body.appendChild(img);
 }
 
 socket.on("yourCards", cards => {
   initArea(cards);
+});
+
+function sendChoice(choice) {
+  socket.emit("setChoice", { choice: choice, player: pseudo });
+}
+
+function displayChoiceButton(disabledChoice) {
+  let div = document.createElement("div");
+  div.className = "priseGrid";
+
+  let choice = ["Passe", "Petite", "Garde", "Garde contre", "Garde sans", "Chelem", "Trier"];
+
+  for (let index in choice) {
+    let button = document.createElement("button");
+    if (choice[index] === "Passe") {
+      button.className = "btn btn-success";
+    } else if (choice[index] === "Trier") {
+      button.className = "btn btn-danger";
+      button.disabled = true;
+    } else if (choice[index] === "Chelem") {
+      button.className = "btn btn-dark";
+    } else {
+      button.className = "btn btn-warning";
+    }
+    if (choice[index] === disabledChoice) {
+      button.disabled = true;
+    }
+    button.innerText = choice[index];
+    button.onclick = () => {
+      sendChoice(choice[index]);
+      div.remove();
+    };
+    button.style.left = (20 + index * 10).toString() + "vw";
+
+    div.appendChild(button);
+  }
+  document.body.appendChild(div);
+}
+
+socket.on("choice", displayChoiceButton);
+
+socket.on("yourTurn", () => {
+  document.getElementsByClassName("area")[0].className += " playerTurn";
+});
+
+socket.on("clean", () => {
+  let timer = setInterval(() => {
+    let cards = document.getElementsByClassName("otherCard");
+    if (cards[0] === undefined) {
+      clearInterval(timer);
+      return;
+    }
+    cards[0].remove();
+  }, 10);
 });
